d1ae8aa8c3834c9b35d02664e0cc69eda22dde0b
Putri Anggia C <p.anggiacahyani@gmail.com>
2024-08-29 13:35:39 +0700

    revision: model, repo, service

---
+++ src/main/java/com/example/demo/service/AssetComponentService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.model.AssetComponent;
+import com.example.demo.service.generic.GenericService;
+
+public interface AssetComponentService extends GenericService<AssetComponent, Integer> {
+    
+}
---
+++ src/main/java/com/example/demo/service/DamageAssessmentService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.model.DamageAssessment;
+import com.example.demo.service.generic.GenericService;
+
+public interface DamageAssessmentService extends GenericService<DamageAssessment, Integer>{
+    
+}
